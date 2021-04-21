180f719843152edb801c0f39cb89d9054777c3e5
KARAN PRAVEEN <karansiamintern@gmail.com>
2021-04-21 13:38:37 +0530

    Basic drop command

---
+++ dropping_in_Mongo.py
@@ -0,0 +1,9 @@
+import pymongo
+from pymongo import MongoClient
+
+my_client = MongoClient("mongodb+srv://karansiamintern:1234@cluster0.4sl4c.mongodb.net/my_database?retryWrites=true&w=majority")
+
+my_db = my_client["my_database"]
+my_collection = my_db["my_collection"]
+
+my_collection.drop()
