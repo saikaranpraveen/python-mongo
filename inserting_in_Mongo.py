8cf10ce631c869755c9573618df016f28018f6c8
KARAN PRAVEEN <karansiamintern@gmail.com>
2021-04-21 13:27:10 +0530

    Inserting data inside a collection

---
+++ inserting_in_Mongo.py
@@ -0,0 +1,11 @@
+import pymongo
+from pymongo import MongoClient
+
+my_client = MongoClient("mongodb+srv://karansiamintern:1234@cluster0.4sl4c.mongodb.net/my_database?retryWrites=true&w=majority")
+
+my_db = my_client["my_database"]
+my_collection = my_db["my_collection"]
+
+my_dict = { "name" : "John Doe", "address" : "Broadway"}
+
+x = my_collection.insert_one(my_dict)
